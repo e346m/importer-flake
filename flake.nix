69fc071ce0ff506a570d6d3f24eda9fe60118f93
Eiji Mishiro <eiji346g@gmail.com>
2024-06-27 20:22:48 +0900

    first commit

---
+++ flake.nix
@@ -0,0 +1,43 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-23.05";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+  outputs = { self, nixpkgs, flake-utils }:
+      flake-utils.lib.eachDefaultSystem (system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+          version = "0.1.4";
+        in
+        with pkgs;
+        {
+          packages = {
+            importer = buildGoModule rec {
+                pname = "importer";
+                inherit version;
+
+                src = fetchFromGitHub {
+                  owner = "upsidr";
+                  repo = "importer";
+                  rev = "v${version}";
+                  sha256 = "sha256-LWWs3zSLlq6P5SFdUbauL/fZPGgUmb2nlkf88Wy1+/M=";
+
+                };
+
+                vendorHash = "sha256-622Dz9m4q6KMXKbMiCKeVfjhdhKX8+TQzZecby3b83s=";
+
+                meta = with lib; {
+                  description = "Import any lines, from anywhere";
+                  homepage = "https://github.com/upsidr/importer/tree/main";
+                  changelog = "https://github.com/upsidr/importer/releases";
+                  license = licenses.asl20;
+                  maintainers = with maintainers; [ e346m ];
+                  mainProgram = "importer";
+                };
+            };
+          };
+
+          defaultPackage = self.packages.${system}.importer;
+        }
+      );
+  }
